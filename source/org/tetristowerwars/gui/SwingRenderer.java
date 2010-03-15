d930ec0c945343e7c63845f954842087a7e00bf4
mikael.hjorth <mikael.hjorth@57f45af4-2c4d-11df-8e42-19cc402ff320>
2010-03-15 14:32:49 +0000

    Now draws joints as well.

--- source/org/tetristowerwars/gui/SwingRenderer.java
+++ source/org/tetristowerwars/gui/SwingRenderer.java
@@ -4,6 +4,7 @@
  */
 package org.tetristowerwars.gui;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -16,6 +17,7 @@ import javax.swing.JPanel;
 import org.jbox2d.collision.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
+import org.tetristowerwars.model.BuildingBlockJoint;
 import org.tetristowerwars.model.GameModel;
 import org.tetristowerwars.model.building.BuildingBlock;
 import org.tetristowerwars.model.material.GroundMaterial;
@@ -66,46 +68,58 @@ public class SwingRenderer extends Renderer {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
+
+            //draw ground
             drawBody(g2, gameModel.getGroundBody(), new GroundMaterial());
 
+            //draw blocks
             for (BuildingBlock block : gameModel.getBlockPool()) {
                 drawBlock(g2, block);
             }
-        }
 
-        private void drawBlock(Graphics2D g, BuildingBlock block)
-        {
-
-            for (Body body : block.getBodies()) {
-                drawBody(g, body, block.getMaterial());
+            //draw joints
+            for (BuildingBlockJoint joint : gameModel.getBuildingBlockJoints()) {
+                drawJoint(g2, joint);
             }
         }
 
-        private void drawBody(Graphics2D g, Body body, Material mat) {
+        private void drawBlock(Graphics2D g2, BuildingBlock block)
+        {
+
+            for (Body body : block.getBodies()) {
+                drawBody(g2, body, block.getMaterial());
+            }
+        }
+
+        private void drawBody(Graphics2D g2, Body body, Material mat) {
             for(PolygonShape shape = (PolygonShape) body.getShapeList(); shape != null; shape = (PolygonShape) shape.m_next) {
 
                 Vec2[] vertices = shape.getVertices();
-                AffineTransform currentTransform = g.getTransform();
+                AffineTransform currentTransform = g2.getTransform();
 
-                g.translate(body.getPosition().x, getHeight() - body.getPosition().y);
-                g.rotate(-body.getAngle());
+                g2.translate(body.getPosition().x, getHeight() - body.getPosition().y);
+                g2.rotate(-body.getAngle());
           
                 //fillPolygon implementation
-                //draws every shape as a rectangles
-                int nPoints = 4;
-                int[] xpoints = new int[nPoints];
-                int[] ypoints = new int[nPoints];
+                int[] xpoints = new int[vertices.length];
+                int[] ypoints = new int[vertices.length];
                 
                 for (int i = 0; i < vertices.length; i++) {
                     xpoints[i] = (int)vertices[i].x;
                     ypoints[i] = -(int)vertices[i].y;
                 }
 
-                g.setColor(mat.getColor()); //color defined by material
-                g.fillPolygon(xpoints, ypoints, nPoints);
+                g2.setColor(mat.getColor()); //color defined by material
+                g2.fillPolygon(xpoints, ypoints, vertices.length);
 
-                g.setTransform(currentTransform);
+                g2.setTransform(currentTransform);
             }
         }
+
+        private void drawJoint(Graphics2D g2, BuildingBlockJoint joint) {
+            g2.setColor(Color.BLACK);
+            g2.drawLine((int)joint.getPointerPosition().x, getHeight()-(int)joint.getPointerPosition().y, (int)joint.getBodyPosition().x, getHeight()-(int)joint.getBodyPosition().y);
+        }
+
     }
 }
