047b842f011fc272642cb6c8b4406f7e9a848706
mikael.hjorth <mikael.hjorth@57f45af4-2c4d-11df-8e42-19cc402ff320>
2010-04-12 15:02:03 +0000

    Added CompoundWinningContion. Changed how WinningConditions are registered with the GameModel, must now be done manually.

---
+++ source/org/tetristowerwars/model/winningcondition/CompoundWinningCondition.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.tetristowerwars.model.winningcondition;
+
+import java.util.List;
+import org.tetristowerwars.model.GameModel;
+import org.tetristowerwars.model.WinningCondition;
+
+/**
+ * Used to group several WinningConditions into one.<p>
+ * Can be set to check if the WinningCondition is fulfilled either by
+ * AND or OR logic.
+ * @author Reeen
+ */
+public class CompoundWinningCondition extends WinningCondition {
+    private final List<WinningCondition> conditions;
+    private final LogicType logicType;
+/**
+ *
+ * @param model The GameModel
+ * @param conditions A List of the WinningConditions
+ * @param logicType An Enum controlling the logic to check the WinningConditions
+ */
+    public CompoundWinningCondition (GameModel model, List<WinningCondition> conditions, LogicType logicType) {
+        super(model);
+        this.conditions = conditions;
+        this.logicType = logicType;
+    }
+
+    /**
+     * Checks if the game is over.<br>
+     * Depending on the LogicType set for this object the WinningConditions are
+     * checked with AND-logic (all must be true) or OR-logic (any must be true).
+     *
+     * @return true if the game is over, else false
+     */
+    @Override
+    public boolean gameIsOver() {
+        switch (logicType) {
+            case AND:
+                for (WinningCondition winningCondition : conditions) {
+                    if  (!winningCondition.gameIsOver()) {
+                        return false;
+                    }
+                }
+                return true;
+
+            case OR:
+                for (WinningCondition winningCondition : conditions) {
+                    if  (winningCondition.gameIsOver()) {
+                        return true;
+                    }
+                }
+                return false;
+
+            default: return false;
+        }
+    }
+
+    public LogicType getLogicType() {
+        return logicType;
+    }
+
+    /**
+     * Can be either AND or OR.
+     */
+    public enum LogicType {
+        AND, OR
+    }
+
+}
