f2d67790129324aeefaff8a9f6f42c652470019a
andreas.duchen <andreas.duchen@57f45af4-2c4d-11df-8e42-19cc402ff320>
2010-04-13 22:19:44 +0000

    background animations are now sorted back-to-front before rendered.

--- source/org/tetristowerwars/gui/gl/BackgroundAnimationRenderer.java
+++ source/org/tetristowerwars/gui/gl/BackgroundAnimationRenderer.java
@@ -10,9 +10,9 @@ import com.sun.opengl.util.texture.TextureIO;
 import java.io.File;
 import java.io.IOException;
 import java.nio.FloatBuffer;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -28,8 +28,7 @@ import static javax.media.opengl.GL.*;
 public class BackgroundAnimationRenderer {
 
     private final Map<Integer, TextureEntry> textures = new HashMap<Integer, TextureEntry>();
-    private final Map<Integer, List<Animation>> animations = new LinkedHashMap<Integer, List<Animation>>();
-    
+    private final List<Animation> animations = new LinkedList<Animation>();
     private FloatBuffer vertexBuffer;
     private FloatBuffer texCoordBuffer;
     public final static int TANK1 = 0, TANK2 = 1, SCUD = 2, ZEPPELIN1 = 3, ZEPPELIN2 = 4, SPUTNIK = 5;
@@ -42,7 +41,7 @@ public class BackgroundAnimationRenderer {
         textures.put(ZEPPELIN1, new TextureEntry(TextureIO.newTexture(new File("res/gfx/decoration/zeppelin1.png"), true), false));
         textures.put(ZEPPELIN2, new TextureEntry(TextureIO.newTexture(new File("res/gfx/decoration/zeppelin2.png"), true), false));
         textures.put(SPUTNIK, new TextureEntry(TextureIO.newTexture(new File("res/gfx/decoration/sputnik.png"), true), true));
-        
+
         for (TextureEntry entry : textures.values()) {
             GLUtil.fixTextureParameters(entry.texture);
         }
@@ -53,12 +52,8 @@ public class BackgroundAnimationRenderer {
 
     public void render(GL gl, float timeElapsed) {
 
-        int numAnimations = 0;
-        for (List<Animation> list : animations.values()) {
-            processAnimation(list, timeElapsed);
-            numAnimations += list.size();
-        }
-
+        processAnimation(animations, timeElapsed);
+        int numAnimations = animations.size();
 
         int numCoords = numAnimations * NUM_VERTICES_PER_ANIMATION;
 
@@ -67,52 +62,50 @@ public class BackgroundAnimationRenderer {
             texCoordBuffer = BufferUtil.newFloatBuffer(numCoords * 2);
         }
 
-        for (Map.Entry<Integer, List<Animation>> entry : animations.entrySet()) {
-            constructBuffers(entry.getValue(), textures.get(entry.getKey()));
-        }
+        Collections.sort(animations); // render back-to-front
+        constructBuffers(animations);
+
 
         vertexBuffer.rewind();
         texCoordBuffer.rewind();
 
         gl.glEnable(GL_TEXTURE_2D);
         gl.glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-        // Draw background
-        // This blend function is needed for photoshop-like blend.
         gl.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 
         gl.glVertexPointer(2, GL_FLOAT, 0, vertexBuffer);
         gl.glTexCoordPointer(2, GL_FLOAT, 0, texCoordBuffer);
 
         int startPos = 0;
 
-        for (Map.Entry<Integer, List<Animation>> entry : animations.entrySet()) {
-            int image = entry.getKey();
-            List<Animation> list = entry.getValue();
-            
-            Texture texture = textures.get(image).texture;
-            texture.bind();
-            gl.glDrawArrays(GL_QUADS, startPos, list.size() * NUM_VERTICES_PER_ANIMATION);
-            startPos += list.size() * NUM_VERTICES_PER_ANIMATION;
+        Texture lastTexture = null;
+        for (Animation animation : animations) {
+            if (lastTexture != animation.textureEntry.texture) {
+                lastTexture = animation.textureEntry.texture;
+                lastTexture.bind();
+            }
+            gl.glDrawArrays(GL_QUADS, startPos, NUM_VERTICES_PER_ANIMATION);
+            startPos += NUM_VERTICES_PER_ANIMATION;
         }
 
         gl.glDisable(GL_TEXTURE_2D);
         gl.glDisableClientState(GL_TEXTURE_COORD_ARRAY);
     }
 
-    private void constructBuffers(List<Animation> animations, TextureEntry textureEntry) {
+    private void constructBuffers(List<Animation> animations) {
         for (Animation animation : animations) {
 
             Vec2 pos = animation.path.getCurrentPosition();
             float halfWidth = animation.width * 0.5f;
-            float halfHeight = halfWidth / textureEntry.texture.getAspectRatio();
-            boolean hMirror = !(animation.path.isLeftToright() ^ textureEntry.isGoingRight);
+            float halfHeight = halfWidth / animation.textureEntry.texture.getAspectRatio();
+            boolean hMirror = !(animation.path.isLeftToright() ^ animation.textureEntry.isGoingRight);
 
             vertexBuffer.put(new float[]{
                         pos.x - halfWidth, pos.y - halfHeight,
                         pos.x + halfWidth, pos.y - halfHeight,
                         pos.x + halfWidth, pos.y + halfHeight,
-                        pos.x - halfWidth, pos.y + halfHeight
-                    });
+                        pos.x - halfWidth, pos.y + halfHeight,});
             if (hMirror) {
                 texCoordBuffer.put(new float[]{
                             0.0f, 1.0f,
@@ -132,16 +125,7 @@ public class BackgroundAnimationRenderer {
     }
 
     public void addAnimation(Path path, float width, int image) {
-
-        List<Animation> list = animations.get(image);
-
-        if (list == null) {
-            list = new LinkedList<Animation>();
-            animations.put(image, list);
-        }
-
-        list.add(new Animation(path, width));
-        
+        animations.add(new Animation(path, width, textures.get(image)));
     }
 
     private void processAnimation(List<Animation> list, float timeElapsed) {
@@ -152,23 +136,35 @@ public class BackgroundAnimationRenderer {
                 it.remove();
             }
         }
-        
-        
     }
 
-    private class Animation {
+    private class Animation implements Comparable<Animation> {
 
         private final Path path;
         private final float width;
+        private final TextureEntry textureEntry;
 
-        public Animation(Path path, float width) {
+        public Animation(Path path, float width, TextureEntry textureEntry) {
             this.path = path;
             this.width = width;
+            this.textureEntry = textureEntry;
         }
 
+        @Override
+        public int compareTo(Animation o) {
+            float diff = width - o.width;
+            if (diff < 0) {
+                return -1;
+            } else if (diff > 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 
     private class TextureEntry {
+
         private final Texture texture;
         private final boolean isGoingRight;
 
